ad7e9ea53316e28ad81f872336eb94ecced2ce2f
James <jamesdaniel2197@gmail.com>
2017-02-09 19:13:50 +0000

    fix wepback dev server settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const path = require('path');
 const config = require('./package.json');
 
@@ -15,18 +17,19 @@ PROD ? [
   ] : '';
 
 module.exports = {
-  entry: path.resolve(__dirname, config.main),
+  entry: ["webpack/hot/dev-server", "./src/iro.es6"],
   devtool: 'source-map',
   output: {
     library: process.env.NAME,
     libraryTarget: process.env.TARGET,
-    path: __dirname,
-    filename: (PROD) ? 'build/iro.min.js' : 'build/iro.js'
+    path: __dirname + "/build",
+    filename: (PROD) ? 'iro.min.js' : 'iro.js',
+    publicPath: "/build/"
   },
   module: {
     loaders: [
       {test: /\.es6?$/, exclude: /node_modules/, loader: 'babel'}
     ]
   },
-  plugins: plugins
+  plugins: plugins,
 };
