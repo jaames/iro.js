d250e5c55a2f284b52b304ff6c126a0b73e71b30
James <jamesdaniel2197@gmail.com>
2017-04-06 12:01:17 +0100

    more cleanup + internal renaming in colorPicker.js
    No external API changes expected

--- src/modules/colorPicker.js
+++ src/modules/colorPicker.js
@@ -6,70 +6,23 @@ import iroColor from "./color.js";
 import iroStyleSheet from "./stylesheet.js";
 
 // When the user starts to interact with a color picker's UI, a referece to that coloPicker will be stored globally
-let active = false;
+let activeColorWheel = false;
 
 // Global mousemove + touchmove event handler
 dom.listen(document, ["mousemove", "touchmove"], function (e) {
   // If there is an active colorWheel, call its mousemove handler
-  if (active) active._mouseMove(e);
+  if (activeColorWheel) activeColorWheel._mouseMove(e);
 });
 
 // Global mouseup + touchend event handler
 dom.listen(document, ["mouseup", "touchend"], function (e) {
   // If there is an active colorWheel, stop it from handling input and clear the active colorWheel reference
-  if (active) {
+  if (activeColorWheel) {
     e.preventDefault();
-    active._mouseTarget = false;
-    active = false;
+    activeColorWheel._mouseTarget = false;
+    activeColorWheel = false;
   }
 });
-
-/**
-  * @desc Handle creating <canvas> elments for the UI
-  * @param {Element} wrapper - DOM element to append all the canvas layers to
-  * @param {Number} width - width of the layers
-  * @param {Number} height - height of the layers
-  * @param {Array} names - an array of layer names
-  * @return {Object} - canvas and ctx objects for each layer, keyed by layer name
-  * @access private
-*/
-function createLayers(wrapper, width, height, names) {
-  // Make sure the canvas wrappe is position:relative
-  // This is because we'll be using position:absolute to stack the canvas layers
-  wrapper.style.cssText += "position:relative";
-  // To support devices with hidpi screens, we scale the canvas so that it has more pixels, but still has the same size visually
-  // This implementation is based on https://www.html5rocks.com/en/tutorials/canvas/hidpi/
-  var pxRatio = devicePixelRatio || 1;
-  // Multiply the visual width and height by the pixel ratio
-  // These dimensions will be used as the internal pixel dimensions for the canvas
-  var pxWidth = width * pxRatio;
-  var pxHeight = height * pxRatio;
-  // When we make new layers we'll add it to this object
-  var ret = {};
-  // Loop through each entry in `names` and create a layer with that name
-  names.forEach(function (name, index) {
-    // Create a new canvas and add it to the page
-    var canvas = dom.append(wrapper, dom.create("canvas"));
-    var ctx = canvas.getContext("2d");
-    var style = canvas.style;
-    // Set the internal dimensions for the canvas
-    canvas.width = pxWidth;
-    canvas.height = pxHeight;
-    // Set the visual dimensions for the canvas
-    style.cssText += "width:" + width + "px;" + "height" + height + "px";
-    // Scale the canvas context to counter the manual scaling of the element
-    ctx.scale(pxRatio, pxRatio);
-    // Since we're creating multiple "layers" from seperate canvas we need them to be visually stacked ontop of eachother
-    // Here, any layer that isn't the first will be forced to the same position relative to their wrapper element
-    // The first layer isn't forced, so the space it takes up will still be considered in page layout
-    if (index != 0) style.cssText += "position:absolute;top:0;left:0";
-    ret[name] = {
-      ctx,
-      canvas
-    };
-  });
-  return ret;
-};
 
 /**
   @constructor color wheel object
@@ -80,12 +33,48 @@ let colorWheel = function (el, opts) {
   if (!(this instanceof colorWheel)) return new colorWheel(el, opts);
   // If `el` is a string, use it to select an Element, else assume it's an element
   el = ("string" == typeof el) ? dom.$(el) : el;
+  // Make sure the canvas wrapper is position:relative
+  // This is because we'll be using position:absolute to stack the canvas layers
+  el.style.cssText += "position:relative";
   // Find the width and height for the UI
   // If not defined in the options, try the HTML width + height attributes of the wrapper, else default to 320
   var width = opts.width || parseInt(dom.attr(el, "width")) || 320;
   var height = opts.height || parseInt(dom.attr(el, "height")) || 320;
   // Create UI layers
-  var layers = createLayers(el, width, height, ["main", "over"]);
+  // To support devices with hidpi screens, we scale the canvas so that it has more pixels, but still has the same size visually
+  // This implementation is based on https://www.html5rocks.com/en/tutorials/canvas/hidpi/
+  var pxRatio = devicePixelRatio || 1;
+  // Multiply the visual width and height by the pixel ratio
+  // These dimensions will be used as the internal pixel dimensions for the canvas
+  var pxWidth = width * pxRatio;
+  var pxHeight = height * pxRatio;
+  // When we make new layers we'll add them to this object
+  var layers = {};
+  var layerNames = ["main", "over"];
+  // Create a layer for each name
+  layerNames.forEach(function (name, index) {
+    // Create a new canvas and add it to the page
+    var canvas = dom.append(el, dom.create("canvas"));
+    var ctx = canvas.getContext("2d");
+    var style = canvas.style;
+    // Set the internal dimensions for the canvas
+    canvas.width = pxWidth;
+    canvas.height = pxHeight;
+    // Set the visual dimensions for the canvas
+    style.cssText += "width:" + width + "px;height:" + height + "px";
+    // Scale the canvas context to counter the manual scaling of the element
+    ctx.scale(pxRatio, pxRatio);
+    // Since we're creating multiple "layers" from seperate canvas we need them to be visually stacked ontop of eachother
+    // Here, any layer that isn't the first will be forced to the same position relative to their wrapper element
+    // The first layer isn't forced, so the space it takes up will still be considered in page layout
+    if (index != 0) style.cssText += "position:absolute;top:0;left:0";
+    layers[name] = {
+      ctx,
+      canvas
+    };
+  });
+  this.el = el;
+  this.layers = layers;
   // Calculate layout variables
   var padding = opts.padding + 2 || 6,
       sliderMargin = opts.sliderMargin || 24,
@@ -115,8 +104,6 @@ let colorWheel = function (el, opts) {
       marker: marker
     })
   ];
-  this.el = el;
-  this.layers = layers;
   // Create an iroStyleSheet for this colorWheel's CSS overrides
   this.stylesheet = new iroStyleSheet(opts.css || opts.styles || undefined);
   // Create an iroColor to store this colorWheel's selected color
@@ -131,7 +118,7 @@ let colorWheel = function (el, opts) {
 
 colorWheel.prototype = {
   /**
-    * @desc Tet a callback function that gets called whenever the selected color changes
+    * @desc Set a callback function that gets called whenever the selected color changes
     * @param {Function} callback The watch callback
     * @param {Boolean} callImmediately set to true if you want to call the callback as soon as it is added
   */
@@ -153,7 +140,7 @@ colorWheel.prototype = {
     * @return {Object} x and y coordinates from the top-left of the UI
     * @access protected
   */
-  _localPoint: function (e) {
+  _getLocalPoint: function (e) {
     // Prevent default event behaviour, like scrolling
     e.preventDefault();
     // Detect if the event is a touch event by checking if it has the `touches` property
@@ -174,7 +161,7 @@ colorWheel.prototype = {
     * @return {Object} x and y coordinates from the top-left of the UI
     * @access protected
   */
-  _input: function (x, y) {
+  _handleInput: function (x, y) {
     // Use the active UI element to handle translating the input to a change in the color
     this.color.set(this._mouseTarget.input(x, y));
   },
@@ -186,7 +173,7 @@ colorWheel.prototype = {
   */
   _mouseDown: function (e) {
     // Get the local-space position of the mouse input
-    var point = this._localPoint(e),
+    var point = this._getLocalPoint(e),
         x = point.x,
         y = point.y;
 
@@ -195,11 +182,11 @@ colorWheel.prototype = {
       // If the element is hit, this means the user has clicked the element and is trying to interact with it
       if (uiElement.checkHit(x, y)) {
         // Set a reference to this colorWheel instance so that the global event handlers know about it
-        active = this;
+        activeColorWheel = this;
         // Set an internal reference to the uiElement being interacted with, for other internal event handlers
         this._mouseTarget = uiElement;
         // Finally, use the position to update the picked color
-        this._input(x, y);
+        this._handleInput(x, y);
       }
     });
   },
@@ -211,11 +198,11 @@ colorWheel.prototype = {
   */
   _mouseMove: function (e) {
     // If there is an active colorWheel (set in _mouseDown) then update the input as the user interacts with it
-    if (active) {
+    if (this == activeColorWheel) {
       // Get the local-space position of the mouse input
-      var point = this._localPoint(e);
+      var point = this._getLocalPoint(e);
       // Use the position to update the picker color
-      this._input(point.x, point.y);
+      this._handleInput(point.x, point.y);
     }
   },
 
