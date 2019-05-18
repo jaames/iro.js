6939aac160f330e62999caf122abd13ed1eae02d
Kaan <99040235@mydavinci.nl>
2019-05-18 18:29:39 +0200

    Adding correct types

--- src/ui/component.ts
+++ src/ui/component.ts
@@ -8,6 +8,20 @@ const EVENT_TOUCHSTART = 'touchstart';
 const EVENT_TOUCHMOVE = 'touchmove';
 const EVENT_TOUCHEND = 'touchend';
 
+enum Event {
+  mouseDown = "mousedown",
+  mouseMove = "mousemove",
+  mouseUp = "mouseup",
+  touchStart = "touchstart",
+  touchMove = "touchstart",
+  TouchEnd = "touchend"
+}
+
+export enum EventResult {
+  start,
+  move,
+  end
+}
 
 interface Props {
   sliderType: any,
@@ -19,6 +33,7 @@ interface Props {
   handleRadius: any,
   borderWidth: any
 }
+
 interface State {
 
 }
@@ -29,7 +44,7 @@ interface State {
  */
 export default abstract class IroComponent extends Component<Props, State> {
   public uid: string;
-  public base: any; //couldnt find it, put this to fix the compile errors #FIX
+  public base: HTMLElement;
 
   constructor(props: any) {
     super(props, {});
@@ -37,46 +52,47 @@ export default abstract class IroComponent extends Component<Props, State> {
     // This can be used to generate unique IDs for gradients, etc
     this.uid = (Math.random() + 1).toString(36).substring(5);
   }
-  
-  //WHERE DOES this.base EVEN COME FROM!?
 
   componentDidMount() {
-    listen(this.base, [EVENT_MOUSEDOWN, EVENT_TOUCHSTART], this, { passive: false });
+    listen(this.base, [Event.mouseDown, Event.touchStart], this, { passive: false });
   }
 
   componentWillUnmount() {
-    unlisten(this.base, [EVENT_MOUSEDOWN, EVENT_TOUCHSTART], this);
+    unlisten(this.base, [Event.mouseDown, Event.touchStart], this);
   }
 
-  abstract handleInput(x: any, y: any, bounds: any, type: any);
+
+
+  abstract handleInput(x: number, y: number, bounds: ClientRect | DOMRect, type: EventResult);
 
   // More info on handleEvent:
   // https://medium.com/@WebReflection/dom-handleevent-a-cross-platform-standard-since-year-2000-5bf17287fd38
   // TL;DR this lets us have a single point of entry for multiple events, and we can avoid callback/binding hell
-  handleEvent(e: any) {
+  handleEvent(e: MouseEvent | TouchEvent) {
     e.preventDefault();
     // Detect if the event is a touch event by checking if it has the `touches` property
     // If it is a touch event, use the first touch input
-    const point = e.touches ? e.changedTouches[0] : e;
+    const point = e instanceof TouchEvent ? e.changedTouches[0] : e;
     const x = point.clientX;
     const y = point.clientY;
+
     // Get the screen position of the component
     const bounds = this.base.getBoundingClientRect();
 
     switch (e.type) {
-      case EVENT_MOUSEDOWN:
-      case EVENT_TOUCHSTART:
-        listen(document, [EVENT_MOUSEMOVE, EVENT_TOUCHMOVE, EVENT_MOUSEUP, EVENT_TOUCHEND], this, { passive: false });
-        this.handleInput(x, y, bounds, 'START');
+      case Event.mouseDown:
+      case Event.touchStart:
+        listen(document, [Event.mouseMove, Event.mouseMove, Event.mouseUp, Event.TouchEnd], this, { passive: false });
+        this.handleInput(x, y, bounds, EventResult.start);
         break;
-      case EVENT_MOUSEMOVE:
-      case EVENT_TOUCHMOVE:
-        this.handleInput(x, y, bounds, 'MOVE');
+      case Event.mouseMove:
+      case Event.touchMove:
+        this.handleInput(x, y, bounds, EventResult.move);
         break;
-      case EVENT_MOUSEUP:
-      case EVENT_TOUCHEND:
-        this.handleInput(x, y, bounds, 'END');
-        unlisten(document, [EVENT_MOUSEMOVE, EVENT_TOUCHMOVE, EVENT_MOUSEUP, EVENT_TOUCHEND], this, { passive: false });
+      case Event.mouseMove:
+      case Event.TouchEnd:
+        this.handleInput(x, y, bounds, EventResult.end);
+        unlisten(document, [Event.mouseMove, Event.mouseMove, Event.mouseUp, Event.TouchEnd], this, { passive: false });
         break;
     }
   }
