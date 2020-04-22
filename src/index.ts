b4bbd8659c17764f3b064bd30e3d3c5de18e70c2
Drzony <drzony@gmail.com>
2020-04-22 22:54:56 +0200

    Fixed TypeScript exports

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,26 @@
-import { h } from 'preact';
+import { h as _h } from 'preact';
 import { IroColor } from '@irojs/iro-core';
-
-import { IroColorPickerWidget } from './ColorPicker';
 import { IroComponentBase } from './ComponentBase';
-import { IroHandle }  from './Handle';
+import { IroHandle } from './Handle';
 import { IroSlider } from './Slider';
 import { IroBox } from './Box';
 import { IroWheel } from './Wheel';
+import { IroColorPicker, IroColorPickerWidget } from './ColorPicker';
 
-export default {
-  Color: IroColor,
-  ColorPicker: IroColorPickerWidget,
-  ui: {
-    h,
-    ComponentBase: IroComponentBase,
-    Handle: IroHandle,
-    Slider: IroSlider,
-    Wheel: IroWheel,
-    Box: IroBox,
-  },
-  version: VERSION,
+namespace iro {
+  export type Color = IroColor;
+  export const Color = IroColor;
+  export type ColorPicker = IroColorPicker;
+  export const ColorPicker = IroColorPickerWidget;
+
+  export namespace ui {
+    export const h = _h;
+    export const ComponentBase = IroComponentBase;
+    export const Handle = IroHandle;
+    export const Slider = IroSlider;
+    export const Wheel = IroWheel;
+    export const Box = IroBox;
+  }
 }
 
+export default iro;
