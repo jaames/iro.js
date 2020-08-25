60a2539f1e88f518efea27c88d290729204e7edb
davidlan <davidlan@tencent.com>
2020-08-25 16:11:36 +0800

    solve the problem that user touching actions doest not work on touching screen PC

--- docs/.vuepress/components/ColorPicker.vue
+++ docs/.vuepress/components/ColorPicker.vue
@@ -73,7 +73,6 @@ export default {
       });
 
       this.colorPicker.on(['color:init', 'input:change'], (color) => {
-        console.log('input:change, colorpicker')
         this.$emit('onColorChange', color);
       });
 
